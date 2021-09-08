785f9ecc88b7d29b3828a745dc7eb4ace2a80ca0
firuza <firya87@gmail.com>
2021-09-08 20:31:53 +0530

    test future and past questions

--- playground/polls/tests.py
+++ playground/polls/tests.py
@@ -65,4 +65,10 @@ class QuestionIndexViewTests(TestCase):
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
-        
+    def test_future_past_questions(self):
+        question = create_question(question_text="Past question", days=-30)
+        create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'], [question]
+        )
