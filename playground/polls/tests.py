66fb236bcaec38d1e6effa1f6f624e4c1d44813b
firuza <firya87@gmail.com>
2021-09-10 16:42:01 +0530

    update polls test to include choices

--- playground/polls/tests.py
+++ playground/polls/tests.py
@@ -60,6 +60,13 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'], [question]
         )
 
+    def test_past_questions_no_choices(self):
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'], []
+        )
+
     def test_future_question(self):
         create_question(question_text="Future question", days=30)
         response = self.client.get(reverse('polls:index'))
