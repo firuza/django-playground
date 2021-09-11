675d34bc37f709e178d60d80bb056e96d1907b17
firuza <firya87@gmail.com>
2021-09-11 15:40:01 +0530

    display foreign key table in the main django admin

--- playground/polls/admin.py
+++ playground/polls/admin.py
@@ -3,11 +3,16 @@ from .models import Question, Choice
 
 # Register your models here.
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Question information', {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'],
+        'classes': ['collapse']
+        }),
     ]
+    inlines = [ChoiceInline]
 admin.site.register(Question, QuestionAdmin)
-
-admin.site.register(Choice)
