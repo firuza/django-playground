528136fdaf5477bb42e6748309d466017ebdf706
firuza <firya87@gmail.com>
2021-09-11 15:32:12 +0530

    create fieldset in django admin

--- playground/polls/admin.py
+++ playground/polls/admin.py
@@ -4,7 +4,10 @@ from .models import Question, Choice
 # Register your models here.
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('Question information', {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(Choice)
